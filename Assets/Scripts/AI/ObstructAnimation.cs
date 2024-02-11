b226306cb4f1e4bc6b5375d4e2f901de7d836ac4
Naomi <111681760+naomuhibu@users.noreply.github.com>
2024-02-11 22:17:18 +1300

    setting up enemy and obstruction
    enemy(destination, input action)
    obstruct animation

---
+++ Assets/Scripts/AI/ObstructAnimation.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleAnimation : MonoBehaviour
+{
+
+    public float speed = .1f;
+    public float strength = 3f;
+
+    private float randomOffset;
+
+    // Use this for initialization
+    void Start()
+    {
+        randomOffset = Random.Range(0f, 2f);
+    }
+
+    // Update is called once per frame
+   void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Sin(Time.time * speed + randomOffset) * strength;
+        transform.position = pos;
+    }
+ }
