56234b8e8406843e05261011eb4ac03d2c196c65
Naomi <111681760+naomuhibu@users.noreply.github.com>
2024-02-19 11:28:07 +1300

    spawning Enemies

---
+++ Assets/Scripts/AI/EnemyManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LostSouls.AI
+{
+    public class EnemyManager : MonoBehaviour
+    {
+        public GameObject Enemy; //prefab
+        public Transform EnemyPlace;
+
+        public float TimeCount;
+
+        private GameObject CurrentEnemy; //reference of Enemy
+
+        private void Update()
+        {
+            if (CurrentEnemy == null)
+            {
+                TimeCount += Time.deltaTime;
+            }
+            if (TimeCount > 3)
+            {
+                CurrentEnemy = Instantiate(Enemy, EnemyPlace.position, Quaternion.identity);
+                TimeCount = 0;
+            }
+        }
+
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player" && CurrentEnemy != null)
+            {
+                Destroy(CurrentEnemy);
+
+            }
+        }
+    }
+}
