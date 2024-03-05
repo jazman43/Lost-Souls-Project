c000b9e132d17a1ed35c89115abf04a6e895f322
jazman43 <supoww@hotmail.co.uk>
2024-03-05 19:25:24 +1300

    some-work-on-menus

---
+++ Assets/Scripts/UI/MainMenu/MoveMenuCam.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+
+
+namespace LostSouls.UI.Menu
+{
+    public class MoveMenuCam : MonoBehaviour
+    {
+        [SerializeField] private float camMoveSpeed = 1f;
+        [SerializeField] private CinemachineVirtualCamera virtualCamera;
+        private CinemachineTrackedDolly trackedDolly;
+
+
+        private void Awake()
+        {
+            trackedDolly = virtualCamera.GetCinemachineComponent<CinemachineTrackedDolly>();
+        }
+
+
+        private void Update()
+        {
+            if(trackedDolly.m_PathPosition >= 1)
+            {
+                trackedDolly.m_PathPosition = 0;
+            }
+            else
+            {
+                trackedDolly.m_PathPosition += camMoveSpeed * Time.deltaTime;
+            }
+
+            
+        }
+    }
+}
