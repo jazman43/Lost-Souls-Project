667fd8440d1e6e37f278c04b64776aff3f86800a
Naomi <111681760+naomuhibu@users.noreply.github.com>
2024-04-13 14:05:48 +1200

    updated prefab and end credits

--- Assets/Scripts/SoundManager/BattleBGM.cs
+++ Assets/Scripts/SoundManager/BattleBGM.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using LostSouls.SoundManager;
 using UnityEngine;
 
-<<<<<<< HEAD:Assets/Scripts/SoundManager/BattleBGM.cs
-namespace LostSouls.DungeonBGM
+
+namespace LostSouls.BattleBGM
 {
 
-    public class DungeonBGM : MonoBehaviour
+    public class BattleBGM : MonoBehaviour
     {
         // Start is called before the first frame update
         void Start()
@@ -20,24 +21,4 @@ namespace LostSouls.DungeonBGM
 
         }
     }
-=======
-
-namespace LostSouls.SoundManager
-{
-
-    public class DungeonBGM : MonoBehaviour
-    {
-        // Start is called before the first frame update
-        void Start()
-        {
-            BGMManager.Instance.PlayBGM(BGMSoundData.BGM.Dungeon);
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-    }
->>>>>>> 11393002072460163574c34820309485021264ab:Assets/Scripts/SoundManager/DungeonBGM.cs
 }
