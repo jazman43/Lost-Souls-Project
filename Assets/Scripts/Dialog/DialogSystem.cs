7215889d1067a6a1827096b9b0faf4b9fec94325
Naomi <111681760+naomuhibu@users.noreply.github.com>
2024-04-13 21:17:30 +1200

    Updated flowchart of Game start

---
+++ Assets/Scripts/Dialog/DialogSystem.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using Fungus;
+using LostSouls.core;
+using Cinemachine;
+
+namespace LostSouls.Dialog
+{
+    public class DialogSystem: MonoBehaviour
+    {
+
+        private PlayerStateMachine playerStateMachine;
+        private CinemachineBrain cinemachineBrain;
+        private bool isFirstDialogShown = false; 
+        private void Start()
+        {
+            playerStateMachine = FindObjectOfType<PlayerStateMachine>();  // Get Player's StateMachine
+            cinemachineBrain = FindObjectOfType<CinemachineBrain>(); //Get Cinemachine
+            if (!isFirstDialogShown)
+            {
+                isFirstDialogShown = true; //Flag for activated dialog
+                SetIsAlreadySaid();
+            }
+                                                                   
+        }
+
+        public void SetMovementLock(bool locked) // Lock or Unlock PlayerMovement
+        {
+            if (playerStateMachine != null && cinemachineBrain != null)
+            {
+                playerStateMachine.enabled = !locked;
+                cinemachineBrain.enabled = !locked;
+            }
+            else
+            {
+                Debug.LogError("Can not find PlayerStateMachine");
+            }
+        }
+        public void SetIsAlreadySaid() // No more opening dialog when its alreay actiivated
+        {
+            GameObject[] dialogs = GameObject.FindGameObjectsWithTag("Dialog");
+            foreach (GameObject dialog in dialogs)
+                {
+                    dialog.SetActive(false);
+                }
+            }
+        }
+}
