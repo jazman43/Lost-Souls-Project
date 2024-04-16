3d24109de14144cc270ea2fef340b4b755efc8df
Naomi <111681760+naomuhibu@users.noreply.github.com>
2024-04-16 15:18:34 +1200

    fixed Enemy and put on navmesh

--- Assets/Scripts/AI/EnemyHealth.cs
+++ Assets/Scripts/AI/EnemyHealth.cs
@@ -8,31 +8,18 @@ namespace LostSouls.AI
 
     public class EnemyHealth : MonoBehaviour
     {
-        [SerializeField] private int enemyHealth = 10; //Enemies max health
-        [SerializeField] private float enemyDissolve;
-        [SerializeField] private string enemyDissolveValue;
-        //[SerializeField] private float maxEnemyDissolve = 0.78f;
-        [SerializeField] private float enemyDissolveSpeed = 0.01f;
-        [SerializeField] private Material material;
-        private void Start()
-        {
-            material.SetFloat(enemyDissolveValue, 0);
+        public int enemyHealth = 10; //Enemies max health
 
-        }
-  
         public void TakeDamage(int damageAmount)
         {
             SFXManager.Instance.PlaySFX(SFXSoundData.SFX.Damage);
             enemyHealth -= damageAmount;
-            enemyDissolve += enemyDissolveSpeed * Time.deltaTime;
-            material.SetFloat(enemyDissolveValue, enemyDissolve);
             Debug.Log(gameObject.name + " took damage. Current health: " + enemyHealth);
 
             if(enemyHealth <= 0)
             {
-                material.SetFloat(enemyDissolveValue, 0);
                 Debug.Log(gameObject.name + " has been destroyed.");
-                Destroy(gameObject, 1f);　// destroy after 1 sec
+                Destroy(gameObject, 0.2f);　// destroy after 2 sec
             }
         }
     }
--- Assets/Scripts/AI/FieldOfView.cs
+++ Assets/Scripts/AI/FieldOfView.cs
@@ -1,4 +1,3 @@
-using LostSouls.combat;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -33,31 +32,25 @@ namespace LostSouls.AI
         private Material _material;
 
         //private Sequence _seq;
-        private void Awake()
-        {
-            animator = GetComponent<Animator>();
-            playerRef = GameObject.FindGameObjectWithTag("Player");
-        }
+
 
         private void Start()
         {
-            
+            playerRef = GameObject.FindGameObjectWithTag("Player");
             StartCoroutine(FOVRoutine());
         }
         private void Update()
         {
-            if (canSeePlayer)
+            if (playerRef != null && canSeePlayer)
             {
                 GetComponent<NavMeshAgent>().SetDestination(playerRef.transform.position);
             }
-
-             animator.SetFloat("Blend",  GetComponent<NavMeshAgent>().velocity.magnitude);
         }
 
         private IEnumerator FOVRoutine()
         {
             WaitForSeconds wait = new WaitForSeconds(0.2f);
-            
+            animator = GetComponent<Animator>();
 
             while (true)
             {
@@ -70,38 +63,37 @@ namespace LostSouls.AI
         {
            
             Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
-            
+
             if (rangeChecks.Length != 0)
             {
-                Debug.Log("enemy to player");
                 Transform target = rangeChecks[0].transform;
                 Vector3 directionToTarget = (target.position - transform.position).normalized;
-                
+
                 if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
                 {
                     float distanceToTarget = Vector3.Distance(transform.position, target.position);
-                    
+
                     if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
                     {
                         canSeePlayer = true;
-                        //animator.SetBool("canSeePlayer", true);
+                        animator.SetBool("canSeePlayer", true);
                     }
                     else
                     {
                         canSeePlayer = false;
-                        //animator.SetBool("canSeePlayer", false);
+                        animator.SetBool("canSeePlayer", false);
                     }
                 }
                 else
                 {
                     canSeePlayer = false;
-                    //animator.SetBool("canSeePlayer", false);
+                    animator.SetBool("canSeePlayer", false);
                 }
             }
             else if (canSeePlayer)
             {
                 canSeePlayer = false;
-                ///animator.SetBool("canSeePlayer", false);
+                animator.SetBool("canSeePlayer", false);
             }
 
         }
@@ -115,11 +107,11 @@ namespace LostSouls.AI
                 animator.SetInteger("AttacKIndex", Random.Range(0, 2));
                 //HitFadeBlink(Color.white);
                 
-                Health enemyHealth = other.gameObject.GetComponent<Health>(); // get the EnemyHealth component from the current enemy
+                EnemyHealth enemyHealth = EnemyManager.CurrentEnemy.GetComponent<EnemyHealth>(); // get the EnemyHealth component from the current enemy
 
                 if (enemyHealth != null)
                 {
-                    enemyHealth.DealDamage(5); // apply damage to the enemy
+                    enemyHealth.TakeDamage(1); // apply damage to the enemy
                     Debug.Log("Player Collided with enemy. damaged enemy.", enemyHealth);
 
                 }
