ee78e1e8a4a4fb74eabc4765045f949171c31efe
Naomi <111681760+naomuhibu@users.noreply.github.com>
2024-04-18 16:54:48 +1200

    updated scenes

---
+++ Assets/Scripts/SceneManagement/ChangeScene.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Fungus;
+
+public class ChangeScene : MonoBehaviour
+{
+    public Flowchart FlowchartPuzzle;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("SavingShild"))
+        {
+            SceneManager.LoadScene("PuzzleDialog");
+        }
+
+    }
+
+    private void OnDialogFinished()
+    {
+        SceneManager.LoadScene("lvl2");
+    }
+}
